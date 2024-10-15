d5d8ec752f2d077d459a4fd435e754fe8a8cac7b
ZoieV2025 <zoie.vitale2025@gbwl.org>
2024-10-15 12:55:38 +0000

    updated java.main

--- Main.java
+++ Main.java
@@ -2,5 +2,29 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+
+      //One = sign ASSIGNS a value to a variable
+      int age = 15;
+      int year = 14;
+
+      //TWO == signs is checking for EQUALITY
+      boolean match = (age == year);
+      System.out.println("Does age amtch year? " + match);
+
+      year = 15; // RE-ASSIGNING
+      System.out.println(age == year); // true
+
+      // != means "Is NOT equal to"
+      System.out.println(age != year); //false
+
+      // RELATIONAL OPERATORS
+      // used in boolean expressions
+      double pi = 3.14159;
+      System.out.println(pi > 0); // checking if positive
+      System.out.println(pi < 0); // checking if negative
+      System.out.println(pi >= 3.14);
+      System.out.println(pi <= 10);
+
+
    }
 }
