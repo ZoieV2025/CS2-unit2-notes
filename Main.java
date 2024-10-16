5814cf6f021c1cc6fa2f23f9246852231e97b2f4
ZoieV2025 <zoie.vitale2025@gbwl.org>
2024-10-16 13:38:02 +0000

    updated java.main

--- Main.java
+++ Main.java
@@ -25,6 +25,47 @@ public class Main {
       System.out.println(pi >= 3.14);
       System.out.println(pi <= 10);
 
+      // IF STATEMENTS
+      boolean isCold = false;{
+      if (isCold == true);
+         System.out.println("Bring a jacket");
+      }
+         System.out.println("Enjoy your walk!");
+
+      // Magic-8 Ball Mini Program
+      // 1. Generate random integer between 1-8
+
+      double randDoub = Math.random() * 8 + 1; // adjust the output range
+      int randInt = (int) randDoub; // CASTING types
+      System.out.println(randInt);
+
+      // 2. Use that numebr in 8 if statements to print a different response
+
+      if (randInt == 1){
+         System.out.println("It is looking good buddy...");
+      }
+      if (randInt == 2){
+         System.out.println("Your future is bright!");
+      }
+      if (randInt == 3){
+         System.out.println("You will enjoy life!");
+      }
+      if (randInt == 4){
+         System.out.println("Happiness is upon you!");
+      }
+      if (randInt == 5){
+         System.out.println("Success is in your future!");
+      }
+      if (randInt == 6){
+         System.out.println("You will succeed!");
+      }
+       if (randInt == 7){
+         System.out.println("You will do great!");
+      }
+      if (randInt == 8){
+         System.out.println("You will do amazing!");
+      }
+
 
    }
 }
